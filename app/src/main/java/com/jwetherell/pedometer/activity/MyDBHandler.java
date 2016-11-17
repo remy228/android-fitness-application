983545da671dc5fb0d2b065145b0d9e3f9a379dd
remy228 <remyamohanan91@gmail.com>
2016-11-16 22:55:21 -0800

    UI User Profile done

---
+++ app/src/main/java/com/jwetherell/pedometer/activity/MyDBHandler.java
@@ -0,0 +1,7 @@
+package com.jwetherell.pedometer.activity;
+
+/**
+ * Created by remya on 11/16/2016.
+ */
+public class MyDBHandler {
+}
--- app/src/main/java/com/jwetherell/pedometer/activity/UserProfile.java
+++ app/src/main/java/com/jwetherell/pedometer/activity/UserProfile.java
@@ -1,11 +1,15 @@
 package com.jwetherell.pedometer.activity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.app.Activity;
 
 import com.jwetherell.pedometer.R;
 
 public class UserProfile extends Activity {
+
+    MyDBHandler myDBHandler;
+    SQLiteDatabase sqLiteDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
